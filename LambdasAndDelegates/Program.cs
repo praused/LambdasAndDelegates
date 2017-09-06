65f1685d8d63807307aed92c5b60daff618e84b6
David Prause <praused@hotmail.com>
2017-09-06 09:53:28 -0400

    4 Lambdas, Action<T> and Func<T,Result>, Getting Started

--- LambdasAndDelegates/Program.cs
+++ LambdasAndDelegates/Program.cs
@@ -15,22 +15,26 @@ namespace LambdasAndDelegates
         {
             
             var worker = new Worker();
-            worker.WorkPerformed += Worker_WorkPerformed;
-            worker.WorkCompleted += Worker_WorkCompleted;
+            worker.WorkPerformed += (s, e) =>
+            {
+                Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+                Console.WriteLine("Still working on " + e.WorkType);
+            };
+            worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
             worker.DoWork(8, WorkType.GenerateReports);
 
             Console.Read();
         }
 
-        static void Worker_WorkCompleted(object sender, EventArgs e)
-        {
-            Console.WriteLine("Worker is done");
-        }
+        //static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Worker is done");
+        //}
 
-        static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
-        }
+        //static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+        //}
 
     }
 }
